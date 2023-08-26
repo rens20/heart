4e4059e5373d061b30c94d4d8e6a3eb383b6eeac
Wasie123 <127023454+rens20@users.noreply.github.com>
2023-08-26 14:16:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const openBtn = document.querySelector(".open-modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-modal-btn");
+
+function openModal() {
+	modal.classList.remove("hide");
+}
+
+function closeModal(e, clickedOutside) {
+	if (clickedOutside) {
+		if (e.target.classList.contains("modal-overlay"))
+			modal.classList.add("hide");
+	} else modal.classList.add("hide");
+}
+
+openBtn.addEventListener("click", openModal);
+modal.addEventListener("click", (e) => closeModal(e, true));
+closeBtn.addEventListener("click", closeModal);
+ 
+
+var hidden = true;
+    function action() {
+        hidden = !hidden;
+        if(hidden) {
+            document.getElementById('toggler').style.visibility = 'hidden';
+        } else {
+           // document.getElementById('togglee').style.visibility = 'visible';
+        }
+    }
+   
+////
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+  
+  heart.innerText = '💗';
+  
+  document.body.appendChild(heart);
+  
+  setTimeout(() => {
+              heart.remove();
+              }, 5000);
+}
+
+setInterval(createHeart, 300);
